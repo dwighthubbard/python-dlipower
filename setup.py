72c718bb979bdd6782dbb408d4fb9d90db46f599
Dwight Hubbard <d@dhub.me>
2012-06-28 02:39:03 -0700

    Fixed the packaging, hopefully correctly Added a link from the module to the bin file dli_power so it can be used for a command line utility as well.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
 setup(
   name="dlipower",
-  version="0.1.5",
+  version="0.1.7",
   author="Dwight Hubbard",
   author_email="dwight@dwighthubbard.com",
   url="http://pypi.python.org/pypi/dlipower/",
   license="LICENSE.txt",
   packages=["dlipower",],
-  scripts=["example.py"],
+  scripts=["example.py","dli_power"],
   long_description=open('README.txt').read(),
   description="Control digital loggers web power switch",
   requires=["pycurl","BeautifulSoup"],
