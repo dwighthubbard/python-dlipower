de2bc6302421440c5c8a420c23dd3b91c2f877e4
Dwight Hubbard <d@dhub.me>
2012-11-17 12:20:05 -0800

    Changes to make the python package creation more automatic

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 from setuptools import setup
 
+def version(filename='package.version',increment_minor=True,increment_major=False):
+  """ 
+  Get the package version, optionally incrementing the minor/major
+  of the version number.
+  """
+  try:
+    old_version=open(filename).readlines()[0].strip()
+  except IOError:
+    old_version="0.0.0"
+  split_old_version=old_version.split('.')
+  if len(split_old_version) and increment_minor:
+    split_old_version[-1]="%d" % (int(split_old_version[-1])+1)
+  if len(split_old_version)>1 and increment_major:
+    split_old_version[-2]="%d" % (int(split_old_version[-2])+1)
+  new_version='.'.join(split_old_version)
+  if increment_minor or increment_major:
+    open(filename,'w').write(new_version+'\n')
+  return new_version
+  
 setup(
   name="dlipower",
-  version="0.2.33",
+  version=version(),
   author="Dwight Hubbard",
   author_email="dwight@dwighthubbard.com",
   url="http://pypi.python.org/pypi/dlipower/",
   license="LICENSE.txt",
   packages=["dlipower",],
-  scripts=["example.py","dlipower/dlipower.py"],
-  long_description=open('docs/dlipower.txt').read(),
+  scripts=["scripts/example.py","scripts/dlipower","scripts/fence_dli"],
+  long_description=open('README.txt').read(),
   description="Control digital loggers web power switch",
   requires=["BeautifulSoup"],
 )
