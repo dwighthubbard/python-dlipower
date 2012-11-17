8d8ff043f22e853347727a12ae942c009cefd4a4
Dwight Hubbard <d@dhub.me>
2012-11-17 00:06:08 -0800

    Documentation updates

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
   name="dlipower",
-  version="0.2.32",
+  version="0.2.33",
   author="Dwight Hubbard",
   author_email="dwight@dwighthubbard.com",
   url="http://pypi.python.org/pypi/dlipower/",
   license="LICENSE.txt",
   packages=["dlipower",],
   scripts=["example.py","dlipower/dlipower.py"],
-  long_description=open('README.txt').read(),
+  long_description=open('docs/dlipower.txt').read(),
   description="Control digital loggers web power switch",
   requires=["BeautifulSoup"],
 )
